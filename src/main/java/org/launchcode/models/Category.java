ad5be587b79e00869415d0c9d544335eb5a0cf56
Brendan <mcb0193@comcast.net>
2017-04-10 18:59:46 -0500

    Create Category class; Refactor Cheese class to use Categories instead of CheeseTypes; Set up relationship between the two classes

--- src/main/java/org/launchcode/models/Category.java
+++ src/main/java/org/launchcode/models/Category.java
@@ -1,10 +1,10 @@
 package org.launchcode.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Category {
@@ -14,8 +14,12 @@ public class Category {
     private int id;
 
     @NotNull
-    @Size(min = 3, max = 13)
+    @Size(min = 3, max = 13, message = "Name must be between 3 and 13 characters long.")
     private String name;
+
+    @OneToMany
+    @JoinColumn(name = "category_id")
+    private List<Cheese> cheeses = new ArrayList<>();
 
     public Category() {}
 
