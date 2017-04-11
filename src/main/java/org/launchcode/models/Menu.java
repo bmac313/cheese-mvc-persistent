8183e4ea86e194300cb22bd657e22ab8cf9c8209
Brendan <mcb0193@comcast.net>
2017-04-10 19:29:20 -0500

    Add fields, constructors, getters / setters, and the addItem method

--- src/main/java/org/launchcode/models/Menu.java
+++ src/main/java/org/launchcode/models/Menu.java
@@ -1,7 +1,47 @@
 package org.launchcode.models;
 
-/**
- * Created by Brendan on 4/10/2017.
- */
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+
+@Entity
 public class Menu {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    @Size(min = 3, max = 15, message = "Name must be between 3 and 15 characters.")
+    private String name;
+
+    @ManyToMany
+    private ArrayList<Cheese> cheeses;
+
+    public Menu(String name) {
+        this.name = name;
+    }
+
+    public Menu() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void addItem(Cheese item) {
+        cheeses.add(item);
+    }
+
 }
